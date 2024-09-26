d7b66160eafdcb3180dd3733a82d919cab26b9f2
Syed Abdal Shah <syedahmad123123shah@gmail.com>
2024-09-26 23:00:11 +0500

    onbarding first maded=

--- GoGreen/views/FirstOnbarding.swift
+++ GoGreen/views/FirstOnbarding.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct FirstOnbarding: View {
     var body: some View {
-        GeometryReader { geo in
+      
             ZStack {
-                
-                Image(.bg).resizable().scaledToFill().ignoresSafeArea().foregroundStyle(.lightGreen)
+                Image(.bg).resizable().scaledToFill().shadow(color:.appGreen.opacity(0.9)  ,radius: 0.2,x: 10,y: 10).ignoresSafeArea()
                 VStack(alignment: .leading) {
-                    Text("Welcome Back").bold().font(.title).foregroundStyle(.appGreen).frame(maxWidth: .infinity,alignment: .leading).padding()
+                    Text("Welcome Back").bold().font(.title).foregroundStyle(.appGreen).frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal)
+                    Text("we’re glad that that you are here..!!").italic().font(.system(size: 20)).foregroundStyle(.appGreen).frame(maxWidth: .infinity,alignment: .leading).padding(.horizontal).padding(.vertical,0.1)
                     Spacer()
-                    
-                }
-            }
+                    Button {
+                        
+                    } label: {
+                        Text("Lets Get Started").bold().foregroundStyle(.white).frame(width: 200,height: 50,alignment: .center)
+                            .background(.darkGreen).clipShape(.rect(cornerRadius:10))
+                    }.frame(maxWidth: .infinity)
+                }.padding(.vertical,50)
         }
     }
 }
